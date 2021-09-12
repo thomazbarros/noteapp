2e0cde65f83391bfcb520d000fc908b2ce40530f
Thomaz Avila Barros <thomazavila@gmail.com>
2021-09-12 14:05:21 -0300

    First challenge is done.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'My name is Thomaz.')
+//fs.writeFileSync('notes.txt', 'My name is Thomaz.  ');
+fs.appendFileSync('notes.txt', 'This is a comment');
